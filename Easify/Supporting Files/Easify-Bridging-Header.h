ee45273a4bc45b8502b7bdc44bbb213717d62ead
Said Ozcan <saidozcn@gmail.com>
2020-05-17 01:58:23 +0200

    add Easify-Bridging-Header.h

---
+++ Easify/Supporting Files/Easify-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Easify-Bridging-Header.h
+//  Easify
+//
+//  Created by Muhammed Said Özcan on 16/05/2020.
+//  Copyright © 2020 Muhammed Said Özcan. All rights reserved.
+//
+
+#ifndef Easify_Bridging_Header_h
+#define Easify_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* Easify_Bridging_Header_h */
