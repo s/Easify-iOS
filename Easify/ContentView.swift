bf3322cceffdd22b5d8a9e9701070a49967990a2
Said Ozcan <saidozcn@gmail.com>
2020-05-16 20:46:41 +0200

    initial commit

---
+++ Easify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Easify
+//
+//  Created by Muhammed Said Özcan on 16/05/2020.
+//  Copyright © 2020 Muhammed Said Özcan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
