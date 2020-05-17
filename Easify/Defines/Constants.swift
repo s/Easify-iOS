1892f4f5d8da330fbdcd7210c32d28c60c4d2fcb
Said Ozcan <saidozcn@gmail.com>
2020-05-17 02:00:44 +0200

    add Constants

---
+++ Easify/Defines/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Easify
+//
+//  Created by Muhammed Said Özcan on 17/05/2020.
+//  Copyright © 2020 Muhammed Said Özcan. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    enum Files: String {
+        case spotifyPlist = "SpotifyCredentials.plist"
+    }
+}
