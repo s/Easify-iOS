22f4fa38c03805aa7380e9c9124d3c40061d4832
Said Ozcan <saidozcn@gmail.com>
2020-05-17 02:03:01 +0200

    add SceneDelegate logic for Spotify bridge

--- Easify/SceneDelegate.swift
+++ Easify/SceneDelegate.swift
@@ -10,9 +10,18 @@ import UIKit
 import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    // MARK: Properties
+    private lazy var serviceProvider: ServiceProvider? = {
+        do {
+            return try ServiceProvider()
+        } catch {
+            print(error)
+            return nil
+        }
+    }()
     var window: UIWindow?
 
+    // MARK: Lifecycle
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -30,32 +39,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url, let appRemote = serviceProvider?.spotifyService.appRemote else {
+            return
+        }
+        let parameters = appRemote.authorizationParameters(from: url)
+        if let accessToken = parameters?[SPTAppRemoteAccessTokenKey] {
+            appRemote.connectionParameters.accessToken = accessToken
+            serviceProvider?.spotifyService.update(accessToken: accessToken)
+        } else if let errorDescription = parameters?[SPTAppRemoteErrorDescriptionKey] {
+            print(errorDescription)
+        }
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        guard serviceProvider?.spotifyService.appRemote.connectionParameters.accessToken != nil else { return }
+        serviceProvider?.spotifyService.appRemote.connect()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        serviceProvider?.spotifyService.appRemote.disconnect()
     }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
 }
