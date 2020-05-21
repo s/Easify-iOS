d882c0f08cb352446a0be08291ea6890524ee330
Said Ozcan <saidozcn@gmail.com>
2020-05-21 19:31:49 +0200

    reorganize frameworks and add SpotifyLogin as internal framework

---
+++ Modules/CoreModules/SpotifyLogin/SpotifyLogin/SpotifyLogin.h
@@ -0,0 +1,19 @@
+//
+//  SpotifyLogin.h
+//  SpotifyLogin
+//
+//  Created by Muhammed Said Özcan on 21/05/2020.
+//  Copyright © 2020 Muhammed Said Özcan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotifyLogin.
+FOUNDATION_EXPORT double SpotifyLoginVersionNumber;
+
+//! Project version string for SpotifyLogin.
+FOUNDATION_EXPORT const unsigned char SpotifyLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotifyLogin/PublicHeader.h>
+
+
